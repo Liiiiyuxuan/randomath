b1ba6da41b794c80607877df286dde0d8bce97ca
Eason Li <liiiiyuxuan@gmail.com>
2024-12-31 21:54:33 +0800

    nim game

---
+++ .history/Zhihu_collection/算法/nim_game/nim_game_3_20241230231309.py
@@ -0,0 +1,25 @@
+# See the ipynb file for more info
+
+import time
+from typing import List
+
+def nim_game(stones: List[int]): 
+    result = 0 
+    for stone in stones:
+        result ^= stone
+
+start_time = time.time()
+print(deep_first_search([5, 3, 6]))
+end_time = time.time()
+
+elapsed_time = end_time - start_time 
+print(f"Elapsed time: {elapsed_time:.6f} seconds.")
+
+# - - - - - - - - - - dividing line - - - - - - - - - - 
+
+start_time = time.time()
+print(deep_first_search([5, 3, 6, 2]))
+end_time = time.time()
+
+elapsed_time = end_time - start_time 
+print(f"Elapsed time: {elapsed_time:.6f} seconds.")
